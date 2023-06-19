c9508f66ef947167528fed99772823eefd1d781e
Samar Chhetri <samarchhetri23@gmail.com>
2023-06-19 16:25:29 +0545

    predict_pipeline

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask,request, render_template
+
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.exception import CustomException
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+application = Flask(__name__)
+
+app = application
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method =='GET':
+        return render_template('home.html')
+    else:
+        data = CustomData(
+            sex = request.form.get('sex'),
+            age = int(request.form.get('age')),
+            bmi = float(request.form.get('bmi')),
+            children = int(request.form.get('children')),
+            smoker = request.form.get('smoker'),
+            region = request.form.get('region')
+        )
+
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+
+        predict_pipeline = PredictPipeline()
+        results = np.round(predict_pipeline.predict(pred_df),2)
+
+        return render_template('home.html', results= results[0])
+    
+
+if __name__ =="__main__":
+    app.run(host='0.0.0.0', debug=True)
+        
